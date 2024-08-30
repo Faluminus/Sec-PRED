009d2a9335d0473ae5672188c33a86935010433c
Faluminus <semradfilip@email.cz>
2024-08-30 04:06:57 -0800

    my own transformer

--- webserver/my_transformer.py
+++ webserver/my_transformer.py
@@ -24,22 +24,40 @@ class PositionalEncoding(nn.Model):
             if len(seq) <= self.max_seq_len:
                 for token_loc in range(len(seq)):
                     for i in range(self.dim/2):
-                        pos_encoded_matrix[token_loc,2*i+1,seq_pos] = math.sin(token_loc/math.pow(self.scalar,2*i/self.dim))
+                        pos_encoded_matrix[token_loc,2*i,seq_pos] = math.sin(token_loc/math.pow(self.scalar,2*i/self.dim))
                         pos_encoded_matrix[token_loc,2*i+1,seq_pos] = math.cos(token_loc/math.pow(self.scalar,2*i/self.dim))
         
         return pos_encoded_matrix
 
 class  MultiheadSelfAttention(nn.Model):
-    def __init__(self,d_model,heads):
+    def __init__(self,d_model,num_heads,dropout=0.0,bias=True,add_bias_kv=False,add_zero_attn=False,kdim=None,vdim=None,batch_first=False,device=None,dtype=None):
         super().__init__(MultiheadSelfAttention)
         self.q_linear = nn.Linear(d_model,d_model)
         self.k_linear = nn.Linear(d_model,d_model)
         self.v_linear = nn.Linear(d_model,d_model)
 
-    def scaled_dot_product_attention(self):
+    def start(self):
+        dot_product = self.q_linear.matmul(self.k_linear.T)
+        scaled_dot_product = 1.0 / math.sqrt(self.d_model) * dot_product
+        attention = torch.softmax(scaled_dot_product,dim=0)
+        results = attention.matmul(self.v_linear)
+        return results
+
+class EncoderLayer(nn.Model):
+    def __init__(self,positional_embedding,multihead_attention):
+        super.__init__(EncoderLayer)
+        self.linear1 = nn.Linear(nn.LayerNorm(torch.add(positional_embedding,multihead_attention)))
+        self.feed_forward = self.feedForwardAndNormalize()
+        self.normalization = nn.LayerNorm(torch.add(self.linear1,self.feed_forward))
         
-        dot_product = torch.matmul(self.q_linear,self.k_linear)
-        scaled = dot_product / math.sqrt()
+
+    def feedForwardAndNormalize(self):
+        relu = nn.ReLU(self.linear1)
+        linear2 = nn.Linear(relu)
+        return linear2
+    
+class DecoderLayer(nn.Model):
+    
     
 
 class Transformer(nn.Model):
