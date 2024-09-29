82ad9564a7bfb9ad0b7293bee29be850e9b44e6f
Faluminus <semradfilip@email.cz>
2024-09-29 16:51:57 +0200

    Started working on 2D modeling lib for protein sec structure

---
+++ djangowebserver/apiserver/secvis/secvis.py
@@ -0,0 +1,30 @@
+import numpy 
+class SecVis():
+    """Returns tensor with coordinates for each structure"""
+    def __init__(self,secondaryStructure:str):
+        self.secStrucArr = secondaryStructure.split('')
+
+class Drawers():
+    def AlphaHelix():
+        pass
+    
+    def BetaBridge():
+        pass
+
+    def BetaLadder():
+        pass
+
+    def GHelix():
+        pass
+
+    def PiHelix():
+        pass
+
+    def KHelix():
+        pass
+
+    def HydrogenBondedTurn():
+        pass
+
+    def Bend():
+        pass
