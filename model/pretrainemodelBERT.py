568a9680a128e44d191f1b04167dbeac31e78bb9
Faluminus <semradfilip@email.cz>
2024-10-02 19:24:55 +0200

    stylistic changes

--- model/pretrainemodelBERT.py
+++ model/pretrainemodelBERT.py
@@ -5,22 +5,22 @@ import torch.optim as optim
 import torch.nn as nn
 import torch
 
-# Load tokenizer and device
+
 tokenizer = AutoTokenizer.from_pretrained("facebook/esm2_t6_8M_UR50D")
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Load and preprocess data
+
 data = pd.read_csv("D:/MyPrograms/Secondary/Sec-PRED/data/raw/ps4dataset.csv")
 
-# Ensure that input and dssp8 columns are lists of strings
+
 src_data = data['input'].astype(str).tolist()
 tgt_data = data['dssp8'].astype(str).tolist()
 
-# Tokenize the data
+
 src_data_tokenized = tokenizer(src_data, return_tensors="pt", padding=True, truncation=True, max_length=500)
 tgt_data_tokenized = tokenizer(tgt_data, return_tensors="pt", padding=True, truncation=True, max_length=500)
 
-# Create Dataset and DataLoader
+
 train_dataset = TensorDataset(
     src_data_tokenized['input_ids'], 
     src_data_tokenized['attention_mask'], 
@@ -30,18 +30,17 @@ train_dataset = TensorDataset(
 
 train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
 
-# Load model and define optimizer and loss
 model = BartForConditionalGeneration.from_pretrained("facebook/bart-large", forced_bos_token_id=tokenizer.bos_token_id).to(device)
 criterion = nn.CrossEntropyLoss(ignore_index=tokenizer.pad_token_id)
 optimizer = optim.AdamW(model.parameters(), lr=0.001, eps=1e-6, weight_decay=0.001)
 
-# Training loop
+
 model.train()
 for epoch in range(20):
     for i,batch in enumerate(train_loader):
         src_train, src_mask, tgt_train, tgt_mask = [x.to(device) for x in batch]
         
-        # Forward pass
+      
         outputs = model(
             input_ids=src_train,
             attention_mask=src_mask,
@@ -49,7 +48,7 @@ for epoch in range(20):
             decoder_attention_mask=tgt_mask
         )
         
-        # Compute loss and perform backprop
+       
         loss = outputs.loss
         loss.backward()
         if i % 128 == 0:
