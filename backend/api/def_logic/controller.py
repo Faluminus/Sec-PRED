0b55ea20a2ddc21b3daf9faba4406753049acb19
Filip Semrad <108215571+Faluminus@users.noreply.github.com>
2025-03-31 18:50:07 +0200

    Backend fix

---
+++ backend/api/def_logic/controller.py
@@ -0,0 +1,45 @@
+from .modela import ModelA
+import json
+from flask import jsonify ,make_response
+import json
+class Controller:
+    def __init__(self):
+        self.amino_acids = ["A", "R", "N", "D", "C", "Q", "E", "G", "H", "I", "L", "K", "M", "F", "P", "S", "T", "W", "Y", "V"]
+        self.model = ModelA()
+    
+    def __data_check(self,data):
+        #Check if key AC exists and if dict is empty
+        if 'AC' not in data.keys():
+            return make_response(jsonify({"output": "Unknown variable provided"}), 400)
+        #Check if AC contains valid aminoacids
+        for i,ac in enumerate(data['AC']):
+            if ac.upper() not in self.amino_acids:
+                error_output = f'Uknown amino acid type used on char:{i}'
+                return make_response(jsonify({"output": "Unknown amino acid type used on char:{i}"}), 422)
+        return None
+
+
+    def do_prediction(self,data):
+        data = json.loads(data.decode('utf-8'))
+        status = self.__data_check(data)
+        if status is not None:
+            return status    
+        amino_acids = data['AC']
+        status, secondary_structure = self.model.check_cache_record(amino_acids=amino_acids)
+        if status:
+            return make_response(jsonify(secondary_structure),202)
+
+        id = self.model.add_empty_cache_record(amino_acids)
+        self.model.push_to_queue(id,json.dumps(amino_acids))
+        return make_response(jsonify({"AC": amino_acids, "PENDING": True, "ID": id}),202)
+    
+
+    def get_by_id(self,id):
+        status, data = self.model.check_cache_record(id)
+        print(status)
+        print(data)
+        if status:
+            return make_response(jsonify(data),200)
+        return make_response(jsonify(data))
+        
+    
---
+++ backend/entrypoint.py
@@ -0,0 +1,46 @@
+from flask import Flask
+from api.def_logic.controller import Controller
+from flask_restful import Api
+from flasgger import Swagger
+from api.threaded_tasks.queue_handler import QueueHandler
+from flask_cors import CORS
+import threading
+app = Flask(__name__)
+cors = CORS(app)
+api = Api(app)
+controller = Controller()
+app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['SWAGGER'] = {
+    'title': 'My API',
+    'uiversion': 3
+}
+
+swagger = Swagger(app)
+
+########################################################################
+
+from api.resources.all_structures import GetAllProteins
+from api.resources.doprediction import DoPrediction
+from api.resources.welcome import Welcome
+from api.resources.get_by_id import GetById
+from api.threaded_tasks.queue_handler import QueueHandler 
+
+GetAllProteins
+GetById
+DoPrediction
+Welcome
+
+api.add_resource(Welcome, '/api')
+api.add_resource(DoPrediction, '/api/do-prediction')
+api.add_resource(GetById, '/api/get-by-id/<id>')
+api.add_resource(GetAllProteins, '/api/get-all-cached-proteins')
+
+
+########################################################################
+
+if __name__ == '__main__':
+    queue_handler = QueueHandler()
+    queue_thread = threading.Thread(target=queue_handler,args=())
+    queue_thread.start()
+    app.run(host='0.0.0.0', port=5001,debug=True) # <----- for production debug FALSE !!!!!!!!!!!
+    
