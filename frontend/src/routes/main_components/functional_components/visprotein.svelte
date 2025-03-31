c3c388ef103559834d9687166cbe2e49766b6b69
Filip Semrad <108215571+Faluminus@users.noreply.github.com>
2025-03-31 18:48:57 +0200

    Frontend fix

---
+++ frontend/src/routes/main_components/functional_components/visprotein.svelte
@@ -0,0 +1,9 @@
+<script>
+    let {protein_code, protein_name, protein_description} = $props()
+</script>
+<div class="flex flex-col w-[400px] h-40 ">
+    <h2 class="text-blue-500 text-[30px] font-bold">{protein_name}</h2>
+    <p class="w-full h-full break-words text-[15px] leading-snug">
+        {protein_description}
+    </p>
+</div>
