b7d4c242c2fb242c0a97fbc75327b07d8abc5fbc
Filip Semrad <108215571+Faluminus@users.noreply.github.com>
2025-04-05 14:49:50 +0200

    Delete scripts directory

--- scripts/dataploting.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import yfinance as yf
-from statsmodels.tsa.seasonal import seasonal_decompose
-from statsmodels.tsa.arima.model import ARIMA
-from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
